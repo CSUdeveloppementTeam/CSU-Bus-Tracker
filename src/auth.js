c6247efa727ae4100c0914c0659053b0d7c31bc3
Jerry-Banga <102557931+Jerry-Banga@users.noreply.github.com>
2022-10-06 10:01:51 +0300

    Update auth.js

--- src/auth.js
+++ src/auth.js
@@ -2,14 +2,23 @@ import {
     createUserWithEmailAndPassword, signInWithEmailAndPassword,
     sendEmailVerification, signOut, updatePassword
 } from "firebase/auth";
+import { writeUserInfo } from "./db_repository";
 
-export let userCredentials
-export const signUp = (auth, { email, password }) => {
+
+let userDetails;
+export const signUp = (auth, user) => {
+   const { email, password, name} = user;
     createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
             // Signed in 
-            userCredentials = userCredential;
+            userDetails = {
+                "email": userCredential.user.email,
+                "name": name,
+                "creationDate": userCredential.user.metadata.creationTime,
+                "key": userCredential.user.uid,
+              }
             console.log("user signed up successfully");
+            sendToUserEmailVerificationLink(auth);
         })
         .catch((error) => {
             const errorCode = error.code;
@@ -18,26 +27,11 @@ export const signUp = (auth, { email, password }) => {
         });
 }
 
-export const signIn = (auth, { email, password }) => {
-    signInWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            // Signed in 
-            const user = userCredential.user;
-            console.log("user signed in successfully" + user);
-            location.replace("../main_page.html");
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log(`errorCode: ${errorCode}, errorMessage: ${errorMessage}`);
-        });
-}
-
-
-export const sendToUserEmailVerificationLink = (auth) => {
+const sendToUserEmailVerificationLink = (auth) => {
     sendEmailVerification(auth.currentUser)
         .then(() => {
             console.log('Verifiction email sent');
+            checkEmailVerification(auth);
         }).catch((error) => {
             const errorCode = error.code;
             const errorMessage = error.message;
@@ -45,10 +39,40 @@ export const sendToUserEmailVerificationLink = (auth) => {
         });
 }
 
+
+function checkEmailVerification(auth, db) {
+    document.getElementById("auth_message").style.display = 'grid';
+    document.getElementById("auth_tutorial").style.display = "block";
+    setTimeout(function(){
+        document.getElementById('auth_message').scrollIntoView();
+    }, 500);
+    setInterval(function () {
+      if (auth.currentUser.emailVerified) {
+        writeUserInfo(db, userDetails);
+        location.replace("../main_page.html"); 
+      }
+    }, 2000);
+  }
+
+export const signIn = (auth, { email, password }) => {
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            console.log("user signed in successfully" + user);
+            location.replace("../main_page.html")
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(`errorCode: ${errorCode}, errorMessage: ${errorMessage}`);
+        });
+}
+
+
 export function signOutUser(auth) {
     signOut(auth).then(() => {
         console.log('Signed Out');
-        location.replace('../login.html');
     }).catch((error) => {
         console.error('Sign Out Error', error);
     });
